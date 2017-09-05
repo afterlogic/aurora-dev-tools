5278c5a3a5bfc4039ea3c539c5fe7bfced715557
vasil <vasil.sokolov@gmail.com>
2017-09-05 13:56:17 +0300

    added version request

--- pack.sh
+++ pack.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
+read -p "Product version: " sVersion
 
-cd ../
+if [ "$sVersion" != "" ]; then
+	cd ../
+	# now=$(date +"%Y-%m-%d")
 
-now=$(date +"%Y-%m-%d")
+	echo $sVersion > VERSION
 
-echo $now > VERSION
+	#empty data folder
+	#zip -r aurora-cloud.zip data modules static system vendor ".htaccess" dav.php common.php index.php LICENSE favicon.ico robots.txt -x ./data/**\*
 
-#empty data folder
-#zip -r aurora-cloud.zip data modules static system vendor ".htaccess" dav.php common.php index.php LICENSE favicon.ico robots.txt -x ./data/**\*
+	#data/settings only
+	zip -r package.zip data/settings/modules modules static system vendor  ".htaccess" dav.php common.php index.php LICENSE VERSION README.md favicon.ico robots.txt composer.json modules.json gulpfile.js pre-config.json -x **/*.bak
 
-#data/settings only
-zip -r package.zip data/settings/modules modules static system vendor  ".htaccess" dav.php common.php index.php LICENSE VERSION README.md favicon.ico robots.txt composer.json modules.json gulpfile.js pre-config.json -x **/*.bak
-
-cd ./dev
+	cd ./dev
+fi
