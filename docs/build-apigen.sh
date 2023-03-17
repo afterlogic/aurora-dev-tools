6ff59e5b6a951468c7511f41f983ae7ce3a7cdbd
Vasil Sokolov <vasil@afterlogic.com>
2023-03-17 14:53:48 +0200

    updated Apigen

--- docs/build-apigen.sh
+++ docs/build-apigen.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 #php apigen.phar generate -s ./../../modules/ -d ./../../docs/api --config ./../../apigen.neon
-php apigen.phar
+php apigen.phar --working-dir ../../
 php update-apigen.php
